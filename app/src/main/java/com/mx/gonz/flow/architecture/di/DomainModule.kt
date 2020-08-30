9da5d9ec09b69cd9678d66eb50ef98eb3baa7b58
Gonzalo Campos <erick.campos@wizeline.com>
2020-08-29 23:46:58 -0500

    Create factory of new use case.

--- app/src/main/java/com/mx/gonz/flow/architecture/di/DomainModule.kt
+++ app/src/main/java/com/mx/gonz/flow/architecture/di/DomainModule.kt
@@ -1,10 +1,15 @@
 package com.mx.gonz.flow.architecture.di
 
+import com.mx.gonz.flow.architecture.domain.interactor.DropStorageUseCase
 import com.mx.gonz.flow.architecture.domain.interactor.ObservePokemonsUseCase
 import org.koin.dsl.module
 
 val domainModule = module {
     factory {
         ObservePokemonsUseCase(get(), get())
+    }
+
+    factory {
+        DropStorageUseCase(get())
     }
 }
--- app/src/main/java/com/mx/gonz/flow/architecture/di/ViewModelsModule.kt
+++ app/src/main/java/com/mx/gonz/flow/architecture/di/ViewModelsModule.kt
@@ -6,6 +6,6 @@ import org.koin.dsl.module
 
 val viewModelsModule = module {
     viewModel {
-        MainViewModel(get())
+        MainViewModel(get(), get())
     }
 }
