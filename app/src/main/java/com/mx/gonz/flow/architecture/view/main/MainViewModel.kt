e8bc7d9ad83fef0d733879c0bb99c94a804ad728
Gonzalo Campos <erick.campos@wizeline.com>
2020-08-29 22:44:42 -0500

    Create main view model.

---
+++ app/src/main/java/com/mx/gonz/flow/architecture/view/main/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.mx.gonz.flow.architecture.view.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import com.mx.gonz.flow.architecture.domain.entity.Pokemon
+import com.mx.gonz.flow.architecture.domain.interactor.ObservePokemonsUseCase
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flowOn
+
+class MainViewModel(
+    private val observePokemons: ObservePokemonsUseCase
+) : ViewModel() {
+
+    val pokemons = liveData {
+        observePokemons(viewModelScope)
+            .flowOn(viewModelScope.coroutineContext)
+            .distinctUntilChanged()
+            .collect {
+                emit(it)
+            }
+    }
+
+    override fun onCleared() {
+        try {
+            viewModelScope.coroutineContext.cancel()
+        } catch (e: Throwable) {
+
+        }
+
+        super.onCleared()
+    }
+}
