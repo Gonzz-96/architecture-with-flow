a8a9a1d8ad5cfe45ecf9460bdd28444211f91432
Gonzalo Campos <erick.campos@wizeline.com>
2020-08-30 00:07:57 -0500

    Create get remote pokemons use case.

---
+++ app/src/main/java/com/mx/gonz/flow/architecture/domain/interactor/GetRemotePokemonsUseCase.kt
@@ -0,0 +1,24 @@
+package com.mx.gonz.flow.architecture.domain.interactor
+
+import com.mx.gonz.flow.architecture.domain.api.LocalPokemonApi
+import com.mx.gonz.flow.architecture.domain.api.RemotePokemonApi
+import kotlinx.coroutines.delay
+import kotlin.random.Random
+
+class GetRemotePokemonsUseCase(
+    private val remotePokemonApi: RemotePokemonApi,
+    private val localPokemonApi: LocalPokemonApi
+) {
+    suspend operator fun invoke() {
+        for (i in 0 until 10) {
+            val pokemonId = Random.nextInt(1, 300)
+            // get pokemon from network
+            remotePokemonApi.getPokemonById(pokemonId).let {
+                // then introduce it in the db
+                localPokemonApi.addNewPokemon(it)
+            }
+            // simulate heavy operation
+            delay(2_000)
+        }
+    }
+}
