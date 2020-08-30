178806075e40c23056ff2ea6f00d828900b3e366
Gonzalo Campos <erick.campos@wizeline.com>
2020-08-29 23:31:48 -0500

    Observe liveData in MainActivity.

--- app/src/main/java/com/mx/gonz/flow/architecture/view/main/MainActivity.kt
+++ app/src/main/java/com/mx/gonz/flow/architecture/view/main/MainActivity.kt
@@ -2,11 +2,33 @@ package com.mx.gonz.flow.architecture.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
 import com.mx.gonz.flow.architecture.R
+import com.mx.gonz.flow.architecture.adapter.PokemonAdapter
+import com.mx.gonz.flow.architecture.databinding.ActivityMainBinding
+import com.mx.gonz.flow.architecture.domain.entity.Pokemon
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel: MainViewModel by viewModel()
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
+
+        binding.rvPokemons.adapter = PokemonAdapter(emptyList())
+
+        observeData()
+    }
+
+    private fun observeData() {
+        mainViewModel.pokemons.observe(this) {
+            (binding.rvPokemons.adapter as? PokemonAdapter)?.updateList(it)
+        }
     }
 }
