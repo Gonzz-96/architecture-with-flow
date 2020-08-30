4f86024b6f3565366a389e3b74db02d4fa03f4d4
Gonzalo Campos <erick.campos@wizeline.com>
2020-08-29 23:46:43 -0500

    Inject new drop storage use case to MainViewModel.

--- app/src/main/java/com/mx/gonz/flow/architecture/view/main/MainViewModel.kt
+++ app/src/main/java/com/mx/gonz/flow/architecture/view/main/MainViewModel.kt
@@ -1,19 +1,20 @@
 package com.mx.gonz.flow.architecture.view.main
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
-import com.mx.gonz.flow.architecture.domain.entity.Pokemon
+import com.mx.gonz.flow.architecture.domain.interactor.DropStorageUseCase
 import com.mx.gonz.flow.architecture.domain.interactor.ObservePokemonsUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
 
 class MainViewModel(
-    private val observePokemons: ObservePokemonsUseCase
+    private val observePokemons: ObservePokemonsUseCase,
+    private val dropStorage: DropStorageUseCase
 ) : ViewModel() {
 
     val pokemons = liveData {
@@ -23,6 +24,10 @@ class MainViewModel(
             .collect {
                 emit(it)
             }
+    }
+
+    fun resetStorage() = viewModelScope.launch {
+        dropStorage()
     }
 
     override fun onCleared() {
