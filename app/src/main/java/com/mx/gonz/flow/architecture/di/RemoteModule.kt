5437c719e261771ad3a637a10e6cb85e0621c79f
Gonzalo Campos <erick.campos@wizeline.com>
2020-08-29 23:30:52 -0500

    Add base url to retrofit builder.

--- app/src/main/java/com/mx/gonz/flow/architecture/di/RemoteModule.kt
+++ app/src/main/java/com/mx/gonz/flow/architecture/di/RemoteModule.kt
@@ -8,12 +8,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 // TODO move to constants
-private const val BASE_URL = "https://pokeapi.co/api/v2/\n"
+private const val BASE_URL = "https://pokeapi.co/api/v2/"
 
 val remoteModule = module {
     single<PokemonService> {
         Retrofit.Builder()
-            .baseUrl("")
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(PokemonService::class.java)
