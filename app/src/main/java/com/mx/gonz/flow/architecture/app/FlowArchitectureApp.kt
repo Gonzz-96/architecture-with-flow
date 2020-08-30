4c0d29ca291b59aa0266ce4c062cdb02b38fc961
Gonzalo Campos <erick.campos@wizeline.com>
2020-08-29 23:30:29 -0500

    Request uses permission for internet usage.

--- app/src/main/java/com/mx/gonz/flow/architecture/app/FlowArchitectureApp.kt
+++ app/src/main/java/com/mx/gonz/flow/architecture/app/FlowArchitectureApp.kt
@@ -3,18 +3,16 @@ package com.mx.gonz.flow.architecture.app
 import android.app.Application
 import com.mx.gonz.flow.architecture.di.*
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class FlowArchitectureApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
         startKoin {
-            androidLogger()
             androidContext(this@FlowArchitectureApp)
             modules(
-                appModule,
                 domainModule,
                 localModule,
                 remoteModule,
