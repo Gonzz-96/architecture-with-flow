8d6ef57ce765a4b3a71e33beb520953fe931a637
Gonzalo Campos <erick.campos@wizeline.com>
2020-08-29 23:03:36 -0500

    Move main activity.

--- app/src/main/java/com/mx/gonz/flow/architecture/view/main/MainActivity.kt
+++ app/src/main/java/com/mx/gonz/flow/architecture/view/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mx.gonz.flow.architecture
+package com.mx.gonz.flow.architecture.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mx.gonz.flow.architecture.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
